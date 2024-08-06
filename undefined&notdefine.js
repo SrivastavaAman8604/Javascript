3a58152a993d8cc39ec7f983dcfb8d2fede458f2
Aman Srivastava <aman.srivastava2639@gmail.com>
2024-08-06 11:33:50 +0530

    Update undefined&notdefine.js

--- undefined&notdefine.js
+++ undefined&notdefine.js
@@ -2,3 +2,4 @@ var a;
 console.log(a) //undefined becoz global execution context hold a = undefined in memory phase
 
 // undefined !== notdefined is true
+// "not defined" typically occurs when trying to access a variable or object property that does not exist.
